5a039aa631193bf6f33bb6f0047a4af176aba6c2
MahdiMp <madanipour66@gmail.com>
2025-05-18 14:09:06 +0330

    added data context.

---
+++ PolarisServer/Infrastructure/Data/ApplicationDataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDataContext : DbContext
+    {
+
+        public ApplicationDataContext(DbContextOptions<ApplicationDataContext>  options) : base(options)
+        {
+            
+        }
+    }
+}
