db49620081e26e4e6803a9c409aae641d89d9670
MahdiMp <madanipour66@gmail.com>
2025-06-02 19:44:09 +0330

    added netwrok info foreignkey

--- PolarisServer/Domain/Entities/User.cs
+++ PolarisServer/Domain/Entities/User.cs
@@ -6,6 +6,8 @@
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public Guid? NetworkInformationId { get; set; }
+        public NetworkMeasurement? NetworkInformation { get; set; }
 
     }
 }
