fd391369fbba349bf3f5e4e48bcfe0f7e67c7a40
MahdiMp <madanipour66@gmail.com>
2025-06-02 19:30:02 +0330

    added network infromation entity

---
+++ PolarisServer/Domain/Entities/NetworkMeasurement.cs
@@ -0,0 +1,26 @@
+﻿namespace Domain.Entities
+{
+    public class NetworkMeasurement
+    {
+        public Guid Id { get; set; }
+        public long TimeStamp { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public string NetworkType { get; set; }
+        public string? PLMNId { get; set; }
+        public int? Lac { get; set; }
+        public int? Tac { get; set; }
+        public int? Rac { get; set; }
+        public string? CellId { get; set; }
+        public int? ARFCN { get; set; }
+        public string? FrequencyBand { get; set; }
+        public double? ActualFrequencyMhz { get; set; }
+        public int SignalStrength { get; set; }
+        public int? RSRP { get; set; }
+        public int? RSRQ { get; set; }
+        public int? RSCP { get; set; }
+        public int? RXLEV { get; set; }
+        public double? ECNO { get; set; }
+
+    }
+}
--- PolarisServer/Infrastructure/Data/ApplicationDataContext.cs
+++ PolarisServer/Infrastructure/Data/ApplicationDataContext.cs
@@ -6,6 +6,7 @@ namespace Infrastructure.Data
     public class ApplicationDataContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<NetworkMeasurement> NetworkMeasurements { get; set; }
         public ApplicationDataContext(DbContextOptions<ApplicationDataContext>  options) : base(options)
         {
             
